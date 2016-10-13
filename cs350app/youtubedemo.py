5e155fbf6de02ee7bda16f71f88ae2023235d7f6
mdroybal <danroybal@yahoo.com>
2016-10-13 14:05:16 -0600

    Here it is

--- cs350app/youtubedemo.py
+++ cs350app/youtubedemo.py
@@ -1,28 +1,46 @@
+#---Monte Roybal---#
+#---Audio_Video_DL---#
+
 import youtube_dl
 import sys
 
 def my_download(d):
     if d['status'] == 'finished':
-        print('File download complete, now converting ......')
-
-ydl_opts = {
-    
-    'format': raw_input("Enter (1) to download a video or (2) to download a song: "), 
-    'key': 'FFmpegExtractAudio',
-
-}
-
-if ydl_opts['format'] == '1':
-    ydl_opts['format'] = 'mp4'
-    
-elif ydl_opts['format'] == '2':
-    ydl_opts['format'] = 'bestaudio/best'
+        print('File download complete, now converting format ......')
 
 while True:
-    url = raw_input("\nPlease enter a url to a video or song you would like to download(Enter -1 to quit): ")
+    opts = {
     
-    if url == 1:           
-        sys.exit("Goodbye")
+        'format': raw_input("\nPlease ENTER (1) for VIDEO download (2) for SONG download or (3) to EXIT: "), 
+        'key': 'FFmpegExtractAudio',
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    }
+
+
+    if opts['format'] == '3':
+        sys.exit("\n****Goodbye****\n")
+
+    if opts['format'] == '2':
+        opts['format'] = raw_input("\nWould you like to download (1) HIGHEST or (2) LOWEST quality audio: ")
+    
+        if opts['format'] == '1':
+            opts['format'] = 'bestaudio'
+    
+        elif opts['format'] == '2': 
+            opts['format'] = 'worstaudio' 
+
+
+    elif opts['format'] == '1':
+        opts['format'] = raw_input("\nWould you like to download (1) HIGHEST or (2) LOWEST quality video: ")
+    
+        if opts['format'] == '1':
+            opts['format'] = '(bestvideo+bestaudio)' 
+
+        elif opts['format'] == '2':
+             opts['format'] = '(worstvideo+worstaudio)'
+
+
+    url = raw_input("\nPlease ENTER the url of the video or song you would like to download: ")
+
+    with youtube_dl.YoutubeDL(opts) as ydl:
         ydl.download([url])
